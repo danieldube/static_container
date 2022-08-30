b0e6e651f4578faaaec43b59cfa48ec1e4354d30
Daniel Dube <daniel.dube@continental-corporation.com>
2022-08-30 23:29:55 +0200

    Implement allocate and deallocate.

--- include/memory.h
+++ include/memory.h
@@ -5,6 +5,8 @@
 
 namespace static_containers {
 
+class Memory;
+
 class Memory {
   Memory(size_t size) : address(nullptr), size(size), is_used(false) {
     address = static_cast<unsigned char *>(malloc(size));
@@ -13,10 +15,33 @@ class Memory {
   }
 
 public:
+  class Token {
+  public:
+    Token(Memory &memory) : memory(memory) {}
+    ~Token() { memory.deallocate(); }
+
+  private:
+    Memory &memory;
+  };
+
   ~Memory() {
     if (address != nullptr)
       free(address);
   }
+
+  Token allocate() {
+    is_used = true;
+    return Token(*this);
+  }
+
+  void deallocate() { is_used = false; }
+
+  bool isUsed() const { return is_used; }
+
+  Memory(const Memory &memory) = delete;
+  Memory(Memory &&memory) = default;
+  Memory &operator=(const Memory &memory) = delete;
+  Memory &operator=(Memory &&memory) = delete;
 
   unsigned char *address;
   size_t size;
--- tests/test_static_vector.cpp
+++ tests/test_static_vector.cpp
@@ -8,3 +8,14 @@ using namespace static_containers;
 TEST_CASE("StaticVector", "[constructor]") { StaticVector<double> vector; }
 
 TEST_CASE("Memory", "[constructor]") { Memory::make_memory(1024); }
+
+TEST_CASE("Memory allocate") {
+  auto memory = Memory::make_memory(1024);
+
+  CHECK(memory.isUsed() == false);
+  {
+    auto token = memory.allocate();
+    CHECK(memory.isUsed() == true);
+  }
+  CHECK(memory.isUsed() == false);
+}
