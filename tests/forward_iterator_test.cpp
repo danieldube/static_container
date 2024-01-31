c06abb229f62ec96b34a66efbc47c0e02534878d
Daniel Dube <daniel.dube@continental-corporation.com>
2024-01-31 01:36:10 +0100

    Implement std::fill based iterator test.

--- tests/forward_iterator_test.cpp
+++ tests/forward_iterator_test.cpp
@@ -5,18 +5,33 @@
 using namespace static_containers;
 
 TEST_CASE("iterator", "[StaticVector]") {
-  std::vector<uint64_t> container;
+  using Container = std::vector<uint64_t>;
+  Container container;
   container.push_back(1);
   container.push_back(2);
 
-  auto iterator =
-      ForwardIterator<std::vector<uint64_t>>::make_iterator(container);
-  auto end =
-      ForwardIterator<std::vector<uint64_t>>::make_end_iterator(container);
+  using Iterator = ForwardIterator<Container>;
+  auto iterator = Iterator::make_iterator(container);
+  auto end = Iterator::make_end_iterator(container);
   CHECK(iterator != end);
   CHECK(*iterator == 1);
   CHECK(*(iterator++) == 1);
   CHECK(iterator != end);
   ++iterator;
   CHECK(iterator == end);
+}
+
+TEST_CASE("std_fill_iterator", "[StaticVector]") {
+  using Container = std::vector<uint64_t>;
+  Container container;
+  container.push_back(1);
+  container.push_back(2);
+  container.push_back(3);
+
+  using Iterator = ForwardIterator<Container>;
+  std::fill(Iterator::make_iterator(container),
+            Iterator::make_end_iterator(container), 42);
+
+  for (const auto element : container)
+    CHECK(element == 42);
 }
